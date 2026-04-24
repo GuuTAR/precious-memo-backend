6b4db4f18cbd6562be13976cab60518f0e32bcf8
GuuTAR <guitar20122012@gmail.com>
2026-04-24 18:04:04 +0700

    Check Production mode

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,12 +13,16 @@ type Config struct {
 }
 
 func Load() (*Config, error) {
-	var err error = godotenv.Load()
+	var err error
+	isProduction := os.Getenv("IS_PRODUCTION")
 
-	if err != nil {
-		log.Println("Warning: .env file not found, using environment variables")
+	if isProduction == "" {
+		var err error = godotenv.Load()
+
+		if err != nil {
+			log.Println("Warning: .env file not found, using environment variables")
+		}
 	}
-
 	var config *Config = &Config{
 		DatabaseURL: os.Getenv("DATABASE_URL"),
 		Port:        os.Getenv("PORT"),
